8bbad9ac2b59dc84ff686855e9c82a64e115a24d
WuDev19 <nguyenvu19a19@gmail>
2026-05-09 09:20:36 +0700

    feat: operator trong js

---
+++ operator.js
@@ -0,0 +1,24 @@
+/**
+ * +
+ * -
+ * *
+ * **: lũy thừa
+ * /
+ * ++
+ * --
+ * %
+ */
+
+var a = 10;
+var b = 20;
+
+console.log(a + b); // 30
+console.log(a - b); // -10
+console.log(a * b); // 200
+console.log(a / b); // 0.5
+console.log(a % b); // 10  
+console.log(a ** 2); // 100
+console.log(a++); // 10, a = 11
+console.log(++a); // 12, a = 12
+console.log(a--); // 12, a = 11
+console.log(--a); // 10, a = 10
