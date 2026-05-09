57f623d0acc868e0d479c2c2cb39fe2d4bf638a3
WuDev19 <nguyenvu19a19@gmail>
2026-05-09 09:21:44 +0700

    feat: string in js

---
+++ string.js
@@ -0,0 +1,17 @@
+var firstName = "John";
+var lastName = "Doe";
+var fullName = firstName + " " + lastName;  
+//string trong js cũng bất biến như trong java
+console.log(`hello mọi người tôi tên là: ${fullName}`); // Output: John Doe
+console.log(fullName.length)
+console.log(fullName.toUpperCase()) // JOHN DOE
+console.log(fullName.toLowerCase()) // john doe
+console.log(fullName.indexOf("o")) // 1
+console.log(fullName.slice(0, 4)) // John
+console.log(fullName.replace("John", "Jane")) // Jane Doe  
+console.log(fullName.split(" ")) // ["John", "Doe"]
+console.log(fullName.includes("Doe")) // true
+console.log(fullName.startsWith("John")) // true
+console.log(fullName.endsWith("Doe")) // true   
+console.log(fullName.concat(" is a developer.")) // John Doe is a developer.
+console.log(fullName.toLocaleLowerCase) // cái này lowercase theo vị trí người dùng
